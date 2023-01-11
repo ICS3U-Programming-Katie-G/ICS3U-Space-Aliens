dced58319c0102dad3502878d44e752a8e7a3c23
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2023-01-11 05:42:08 +0000

    said hello and introduced the game :)

---
+++ code.py
@@ -0,0 +1,15 @@
+# this is the wonderful jon's joyful jog video game :)
+
+
+def main():
+    print("Hello, World!")
+
+    # describes how great the game is going to be :)
+    print(
+        "This will be a very good game (hopefully) about this man named Jon "
+        "who will be going on the best, most eventful jog of his life"
+    )
+
+
+if __name__ == "__main__":
+    main()
