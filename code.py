f57ffa24d345fdfd5c5f8a9d793e9924871984b5
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2023-01-12 18:46:21 +0000

    here

--- code.py
+++ code.py
@@ -1,15 +1,24 @@
+# Created by: Katie Green
+# Created: January 2023
 # this is the wonderful jon's joyful jog video game :)
 
+# importing some libraries to help build our game.
+import stage
+import ugame
 
-def main():
-    print("Hello, World!")
 
-    # describes how great the game is going to be :)
-    print(
-        "This will be a very good game (hopefully) about this man named Jon "
-        "who will be going on the best, most eventful jog of his life"
-    )
+# this is where all the game environment will be kept.
+def game_scene():
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+
+    while True:
+        pass
 
 
 if __name__ == "__main__":
-    main()
+    game_scene()
