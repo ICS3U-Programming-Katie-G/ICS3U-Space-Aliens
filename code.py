3f7e9887dfe6111066c95320c6be014bc4c41e6c
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2023-01-12 19:33:14 +0000

    ITS JON

--- code.py
+++ code.py
@@ -9,15 +9,34 @@ import ugame
 
 # this is where all the game environment will be kept.
 def game_scene():
+    # the image bank for the background and sprites
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    
+    # creates a grid that the background exists in.
     background = stage.Grid(image_bank_background, 10, 8)
-
+    
+    # this is the sprite of jon, who is the playable character
+    jon = stage.Sprite(image_bank_sprites, 4, 75, 66)
+    
+    
+    # actually displaying the game
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    
+    # the two different layers of the game
+    game.layers = [jon] + [background]
+    
+    # rendering the background
     game.render_block()
 
     while True:
-        pass
+        # will be getting user input
+        
+        # will be updating game logic
+        
+        # redrawing / updating sprites
+        game.render_sprites([jon])
+        game.tick()
 
 
 if __name__ == "__main__":
