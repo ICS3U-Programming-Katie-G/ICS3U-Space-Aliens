263ccd59d88eecb40f8a583d009a2c6f7dc1eac8
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2023-01-13 09:42:06 +0000

    linter errors woooo

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Created by: Katie Green
 # Created: January 2023
 # this is the wonderful jon's joyful jog video game :)
@@ -5,7 +7,6 @@
 # importing some libraries to help build our game.
 import stage
 import ugame
-
 
 # this is where all the game environment will be kept.
 def game_scene():
@@ -31,31 +32,38 @@ def game_scene():
     while True:
         # getting user input.
         keys = ugame.buttons.get_pressed()
-        
+
         # X is a constant that represents A.
         if keys & ugame.K_X:
             print("A")
+
         # O is a constant that represents B.
         if keys & ugame.K_O:
             print("B")
+
         # the start button.
         if keys & ugame.K_START:
             print("Start")
+
         # the select button.
         if keys & ugame.K_SELECT:
             print("Select")
+
         # the right button on the d-pad.
         if keys & ugame.K_RIGHT:
             # moves one pixel right, leaves y as is.
             jon.move(jon.x + 1, jon.y)
+
         # the left button on the d-pad.
         if keys & ugame.K_LEFT:
             # moves one pixel left, leaves y as is.
             jon.move(jon.x - 1, jon.y)
+
         # the up button on the d-pad
         if keys & ugame.K_UP:
             # moves one pixel up, leaves x as is.
             jon.move(jon.x, jon.y - 1)
+
         # the down button on the d-pad.
         if keys & ugame.K_DOWN:
             # moves one pixel down, leaves y as is.
---
+++ constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Katie
+# Created on: January 2023
+# This file contains all the constants for code.py
+
+# The PyBadge screen size is 160x128, and sprites are 16x16.
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+# the frames per second is 60, and the sprites move 1 pixel each time they move.
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
