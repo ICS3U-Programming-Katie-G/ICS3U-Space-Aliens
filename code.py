716c4df0918341bcba895edc9e0bb4a53e3dabcd
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2023-01-13 06:28:42 +0000

    fixed indentation for linter

--- code.py
+++ code.py
@@ -12,28 +12,27 @@ def game_scene():
     # the image bank for the background and sprites
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
-    
+
     # creates a grid that the background exists in.
     background = stage.Grid(image_bank_background, 10, 8)
-    
+
     # this is the sprite of jon, who is the playable character
     jon = stage.Sprite(image_bank_sprites, 4, 75, 66)
-    
-    
+
     # actually displaying the game
     game = stage.Stage(ugame.display, 60)
-    
+
     # the two different layers of the game
     game.layers = [jon] + [background]
-    
+
     # rendering the background
     game.render_block()
 
     while True:
         # will be getting user input
-        
+
         # will be updating game logic
-        
+
         # redrawing / updating sprites
         game.render_sprites([jon])
         game.tick()
