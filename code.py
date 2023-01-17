190b198fa77c834f908917bbe5ded43c145f56e7
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2023-01-17 19:31:57 +0000

    HANK ANDERSON GAME STUDIOS WOWOOW ]

--- code.py
+++ code.py
@@ -17,9 +17,11 @@ def menu_scene():
 
     # adding text objects
     text = []
-    
+
     # this allows us to actually print out the text
-    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1 = stage.Text(
+            width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
 
     # this is where the text will appear
     text1.move(20, 10)
@@ -31,7 +33,9 @@ def menu_scene():
     text.append(text1)
 
     # this is the secondary text, to tell the user to press start!
-    text2 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text2 = stage.Text(
+            width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
 
     # this positions the text
     text2.move(20, 100)
@@ -45,7 +49,6 @@ def menu_scene():
     # this will set the background to image 0 in our image bank,
     # and it will make the size 16x16, tiles of 10x8.
     background = stage.Grid(image_bank_background, 10, 8)
-
 
     # creates a grid that the background exists in.
     background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
@@ -71,6 +74,7 @@ def menu_scene():
         # redrawing
         game.tick()
 
+
 # this is where all the game environment will be kept.
 def game_scene():
     # the image bank for the background and sprites
@@ -84,22 +88,32 @@ def game_scene():
     select_button = constants.button_state["button_up"]
 
     # loading in the song and getting it all ready :)
-    meow_sound = open("meow_sound.wav", 'rb')
+    meow_sound = open("meow_sound.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
 
     # creates a grid that the background exists in.
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    background = stage.Grid(
+            image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
 
     # this is the sprite of jon, who is the playable character
     # this locks the sprite's y position at the near-bottom of the screen.
-    jon = stage.Sprite(image_bank_sprites, 4, 75, constants.SCREEN_Y - (constants.ADDED_BOTTOM + constants.SPRITE_SIZE))
+    jon = stage.Sprite(
+            image_bank_sprites,
+            4,
+            75,
+            constants.SCREEN_Y - (constants.ADDED_BOTTOM + constants.SPRITE_SIZE)
+    )
 
     # this is the sprite of the enemy in the game: lasagna
-    lasagna = stage.Sprite(image_bank_sprites, 9,
-                    int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
-                    18)
+    lasagna = stage.Sprite(
+            image_bank_sprites,
+            9,
+            int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
+            18
+    )
 
     # actually displaying the game
     game = stage.Stage(ugame.display, constants.FPS)
